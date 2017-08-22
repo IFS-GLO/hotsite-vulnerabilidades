806422879952bfcc475640d3c2978f7ebd1b0af2
Raphael F <raphael.fontes@ifs.edu.br>
2017-08-22 15:38:57 -0300

    Arquivos restantes da migração 026eff0

--- hotsite/hotsite/core/urls.py
+++ hotsite/hotsite/core/urls.py
@@ -4,4 +4,12 @@ from .views import *
 
 urlpatterns = [
     url('^$', index, name='index'),
+
+    url('^vulnerability/(?P<pk>\w+)/$', vulnerability, name="vulnerability"),
+
+    url('^providers/$', providers, name="providers"),
+    url('^provider/(?P<pk>\w+)/$', provider, name="provider"),
+
+    url('^products/$', products, name="products"),
+    url('^product/(?P<pk>\w+)/$', product, name="product"),
 ]
--- hotsite/hotsite/core/views.py
+++ hotsite/hotsite/core/views.py
@@ -1,22 +1,104 @@
+from django.db.models import Count
 from django.shortcuts import render
+
+from hotsite.catalog.models import Vulnerability, Provider, Software
 
 
 def index(request):
     template_name = 'hotsite/index.html'
+    title = 'Vulnerabilidades'
 
-    tab = request.GET.get('tab', '')
-
-    if tab == '':
-        title = 'Vulnerabilidades'
-
-    elif tab == 'provider':
-        title = 'Fornecedores'
-
-    elif tab == 'software':
-        title = 'Produtos'
+    instances = Vulnerability.objects.all()
 
     context = {
-        'title': title
+        'title': title,
+        'instances': instances
+    }
+
+    return render(request, template_name, context)
+
+
+def providers(request):
+    template_name = 'hotsite/providers.html'
+    title = 'Fornecedores'
+
+    instances = Provider.objects.all()
+    arr_providers = []
+
+    for instance in instances:
+        # TODO: Get the product with the most vulnerabilities registered and the last update
+
+        provider = {
+            'provider': instance,
+            'products': Software.objects.filter(provider=instance),
+        }
+
+        arr_providers.append(provider)
+
+    instances = arr_providers
+
+    context = {
+        'title': title,
+        'instances': instances
+    }
+
+    return render(request, template_name, context)
+
+
+def provider(request, pk):
+    template_name = 'hotsite/provider.html'
+
+    instance = Provider.objects.get(pk=pk)
+    instances = Software.objects.filter(provider=instance).annotate(vulnerabilities=Count('vulnerability'))
+
+    context = {
+        'head_title': instance.name,
+        'title': instance.name,
+        'instance': instance,
+        'instances': instances
+    }
+
+    return render(request, template_name, context)
+
+
+def products(request):
+    template_name = 'hotsite/products.html'
+    title = 'Produtos'
+
+    instances = Software.objects.all().annotate(vulnerabilities=Count('vulnerability'))
+
+    context = {
+        'title': title,
+        'instances': instances
+    }
+
+    return render(request, template_name, context)
+
+
+def product(request, pk):
+    template_name = 'hotsite/product.html'
+
+    instance = Software.objects.get(pk=pk)
+    instances = Vulnerability.objects.filter(products=instance)
+
+    context = {
+        'head_title': instance.name,
+        'title': instance.name,
+        'instances': instances,
+    }
+
+    return render(request, template_name, context)
+
+
+def vulnerability(request, pk):
+    template_name = 'hotsite/vulnerability.html'
+
+    instance = Vulnerability.objects.get(pk=pk)
+
+    context = {
+        'head_title': instance.name + ' | ',
+        'title': instance.name,
+        'instance': instance
     }
 
     return render(request, template_name, context)
