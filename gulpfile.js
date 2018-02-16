7fa105c1687152d4a9b85ebeccb1ab512f0e83a0
Raphael F <raphael.fontes@ifs.edu.br>
2018-02-16 08:32:27 -0300

    Core
    Criação do arquivo gulp
    Adicionadas dependencias do desenvolvimento
    Correção do caminho dos arquivos de preparacao do ambiente

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+
+var path = {
+  'node': 'node_modules',
+  'dist': 'hotsite/hotsite/core/static'
+}
+
+// scripts paths
+var jsFiles = [
+  path.node + '/tether/dist/js/tether.min.js',
+  path.node + '/jquery/dist/jquery.min.js',
+  path.node + '/popper.js/dist/umd/popper.min.js',
+  path.node + '/bootstrap/dist/js/bootstrap.min.js',
+  path.node + '/datatables.net/js/jquery.dataTables.js',
+  path.node + '/datatables.net-bs/js/dataTables.bootstrap.js',
+  path.node + '/pace-js/pace.min.js',
+  path.node + '/select2/dist/js/select2.min.js',
+  path.node + '/toastr/build/toastr.min.js',
+]
+
+// styles path
+var cssFiles = [
+  path.node + '/bootstrap/dist/css/bootstrap.min.css',
+  path.node + '/tether/dist/css/tether.min.css',
+  path.node + '/font-awesome/css/font-awesome.min.css',
+  path.node + '/datatables.net-bs/css/dataTables.bootstrap.css',
+  path.node + '/select2/dist/css/select2.min.css',
+  path.node + '/toastr/build/toastr.min.css',
+]
+
+gulp.task('scripts', function () {
+  return gulp.src(jsFiles)
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(path.dist + '/js'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src(cssFiles)
+    .pipe(concat('all.min.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest(path.dist + '/css'))
+});
+
+gulp.task('fonts', function () {
+  gulp.src(path.node + '/font-awesome/fonts/**.*')
+    .pipe(gulp.dest(path.dist + '/fonts'));
+});
+
+gulp.task('default', ['styles', 'scripts', 'fonts']);
