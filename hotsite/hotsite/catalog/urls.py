4804e513be2e6a26e0e05b988f9d7b8a1e6f0464
Raphael F <raphael.fontes@ifs.edu.br>
2019-07-05 09:57:03 -0300

    Catalog
    Implementada remoção dos objetos

--- hotsite/hotsite/catalog/urls.py
+++ hotsite/hotsite/catalog/urls.py
@@ -10,21 +10,26 @@ urlpatterns = [
     url('^providers/$', providers, name='providers'),
     url('^provider/$', add_provider, name='add_provider'),
     url('^provider/(?P<pk>\w+)/$', provider, name='provider'),
+    url('^provider/(?P<pk>\w+)/del/$', del_provider, name="del_provider"),
 
     url('^categories/$', categories, name='categories'),
     url('^category/$', add_category, name='add_category'),
     url('^category/(?P<pk>\w+)/$', category, name='category'),
+    url('^category/(?P<pk>\w+)/del/$', del_category, name="del_category"),
 
     url('^softwares/$', softwares, name='softwares'),
     url('^software/$', add_software, name='add_software'),
     url('^software/(?P<pk>\w+)/$', software, name='software'),
+    url('^software/(?P<pk>\w+)/del/$', del_software, name="del_software"),
 
-    url('^vulnerabilitis/$', vulnerabilities, name='vulnerabilities'),
+    url('^vulnerabilities/$', vulnerabilities, name='vulnerabilities'),
     url('^vulnerability/$', add_vulnerability, name='add_vulnerability'),
     url('^vulnerability/(?P<pk>\w+)/$', vulnerability, name='vulnerability'),
+    url('^vulnerability/(?P<pk>\w+)/del/$', del_vulnerability, name='del_vulnerability'),
 
     url('^licenses/$', licenses, name='licenses'),
     url('^license/$', add_license, name='add_license'),
     url('^license/(?P<pk>\w+)/$', license, name='license'),
+    url('^license/(?P<pk>\w+)/del/$', del_license, name='del_license'),
 
 ]
--- hotsite/hotsite/core/urls.py
+++ hotsite/hotsite/core/urls.py
@@ -8,11 +8,12 @@ from .views import *
 app_name = 'core'
 
 urlpatterns = [
-    url('^$', index, name='index'),
+    url('^$', catalog, name='index'),
     url('^login/$', login, name='login'),
 
     url('^catalog/$', catalog, name='catalog'),
 
+    url('^vulnerability/$', index, name='vulnerabilities'),
     url('^vulnerability/(?P<pk>\w+)/$', vulnerability, name="vulnerability"),
 
     url('^providers/$', providers, name="providers"),
