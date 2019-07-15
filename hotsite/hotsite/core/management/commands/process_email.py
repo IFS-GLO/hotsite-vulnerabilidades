3dcd0406b5a9d87a37c441f5cc774be1aaf6f2f8
Raphael F <raphael.fontes@ifs.edu.br>
2019-07-15 10:19:29 -0300

    Send mail
    Implementação do envio de e-mails

--- hotsite/hotsite/core/management/commands/process_email.py
+++ hotsite/hotsite/core/management/commands/process_email.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
+
 from hotsite.core.utils import send_email
+from hotsite.catalog.models import Software
 
 
 class Command(BaseCommand):
-    title = 'Process emails'
+    title = 'Process Emails'
     help = 'Sends emails'
 
     def add_arguments(self, parser):
         pass
 
     def handle(self, *args, **options):
-        send_email(['raphael.fontes@ifs.edu.br'],
+        send_email(['rafael.oliva@ifs.edu.br'],
                    'Resumo de Alerta de Segurança - 09/07/2019', 'panel/mail.html')
         self.stdout.write(self.style.SUCCESS('Successfully emails sent!'))
